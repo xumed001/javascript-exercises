913d98c58edbe6c261d6dceae3ef1b867eb96de8
Sumed Tamang <xumed001@gmail.com>
2023-03-08 20:45:44 -0600

    3/8/23

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  // if (x === 0){
+  //   return 1
+  // } else {
+  //   let array = [...Array(x+1).keys()]
+  //   return array.slice(1).reduce((a, b) => a * b)
+  // }
+
+  return x === 0 ? 1 : [...Array(x+1).keys()].slice(1).reduce((a, b) => a * b)
+
+  // RECURSICE algo
+  // const recursiveFactorial = function (n) {
+  //   if (n === 0) {
+  //     return 1;
+  //   }
+  //   return n * recursiveFactorial(n - 1);
+  // };
 };
 
 // Do not edit below this line
