e9601b48e6bf33cdad023d4b3b4d72f711c93ae5
Sumed Tamang <xumed001@gmail.com>
2023-03-08 21:24:32 -0600

    3/8/23

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let newWord = word.replace(/[^a-z]/gi, '').toLowerCase()
+    return newWord === newWord.split('').reverse().join('')
 };
 
 // Do not edit below this line
