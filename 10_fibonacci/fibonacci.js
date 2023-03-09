6792be5b11575b07e323da08384584b95ecc3094
Sumed Tamang <xumed001@gmail.com>
2023-03-09 11:02:41 -0600

    3/9/23

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return ("OOPS")
 
+    let fib = [0, 1]
+    for (let i = 2; i <= index; i++){
+        fib[i] = fib[i-2] + fib[i - 1]
+    }
+    return fib[index]
 };
 
 // Do not edit below this line
