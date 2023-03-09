c125ee34ba907547f4775cc72d43a2bb05624e39
Sumed Tamang <xumed001@gmail.com>
2023-03-09 11:17:45 -0600

    3/9/23

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    // let newArr = []
+    // arr.forEach((item) => {
+    //     newArr.push(item.title)
+    // })
+    // return newArr
+    return arr.map((item) => item.title)
 };
 
 // Do not edit below this line
